a72d538329b461991f33ff6c727a7bc4c70ee569
Erica Sosa <ericasosa0@gmail.com>
2018-06-14 20:29:26 -0500

    toggle read more expansion on blog and testimonials

--- js/creative.js
+++ js/creative.js
@@ -49,4 +49,40 @@
         $(".portfolio-box-caption").css("opacity", "1");
     });
 
+     //toggle read more on blog
+
+     $('#about-blog').on('shown.bs.collapse', function () {
+        $("#read-about-blog").html('Read less about my blog')
+    });
+
+    $('#about-blog').on('hidden.bs.collapse', function () {
+        $("#read-about-blog").html('Read more about my blog')
+    });
+
+    //toggle read more on testimonials
+
+    $('#suzanne-testimonial').on('shown.bs.collapse', function () {
+        $("#see-more-suzanne").html('See less').insertAfter('#suzanne-testimonial')
+    });
+
+    $('#suzanne-testimonial').on('hidden.bs.collapse', function () {
+        $("#see-more-suzanne").html('See more')
+    });
+
+    $('#llorenc-testimonial').on('shown.bs.collapse', function () {
+        $("#see-more-llorenc").html('See less').insertAfter('#llorenc-testimonial')
+    });
+
+    $('#llorenc-testimonial').on('hidden.bs.collapse', function () {
+        $("#see-more-llorenc").html('See more')
+    });
+
+    $('#alberto-testimonial').on('shown.bs.collapse', function () {
+        $("#see-more-alberto").html('See less').insertAfter('#alberto-testimonial')
+    });
+
+    $('#alberto-testimonial').on('hidden.bs.collapse', function () {
+        $("#see-more-alberto").html('See more')
+    });
+
 })(jQuery); // End of use strict
